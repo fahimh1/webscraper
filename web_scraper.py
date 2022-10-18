ae9bb24b5b48ac5ca9204233659bc6c88990a119
Fahim <104360111+fahimhasan1@users.noreply.github.com>
2022-10-18 18:39:53 +0100

    main program

---
+++ web_scraper.py
@@ -0,0 +1,35 @@
+import bs4
+import requests
+
+# Create a url without pager number
+basic_url = 'http://books.toscrape.com/catalogue/page-{}.html'
+
+# List of 4 or 5 star titles
+high_ratted_titles = []
+
+# Iterate pages
+for page in range(1, 51):
+
+    # Create soup on each page
+    url_page = basic_url.format(page)
+    result = requests.get(url_page)
+    soup = bs4.BeautifulSoup(result.text, 'html.parser')
+
+    # Select data of books
+    books = soup.select('.product_pod')
+
+    # Iterate books
+    for book in books:
+
+        # Check if they are 4 or 5 star books
+        if len(book.select('.star-rating.Four')) != 0 or len(book.select('.star-rating.Five')) != 0:
+
+            # Store title in variable
+            book_title = book.select('a')[1]['title']
+
+            # Add book to list
+            high_ratted_titles.append(book_title)
+
+# Show 4 or 5 star books in console
+for books in high_ratted_titles:
+    print(books)
